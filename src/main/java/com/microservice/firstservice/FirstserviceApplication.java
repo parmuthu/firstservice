a15e944bf8c49167cb45c47ade6a34fcbdf6a6d7
parmuthu <parmuthu2002@gmail.com>
2024-04-21 00:49:49 +0530

    main application modified

--- src/main/java/com/microservice/firstservice/FirstserviceApplication.java
+++ src/main/java/com/microservice/firstservice/FirstserviceApplication.java
@@ -8,7 +8,7 @@ public class FirstserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstserviceApplication.class, args);
-		Syste.out.println("Application started");
+		System.out.println("Application started");
 	}
 
 }
